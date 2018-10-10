d8d2685e4a826b8afb205de8462840e35c1d4b36
Italo Menna Silveira <italo.menna.silveira@gmail.com>
2018-10-10 13:31:32 -0300

    fix call functions on client

--- lib/fieldview/client.rb
+++ lib/fieldview/client.rb
@@ -6,7 +6,19 @@ module Fieldview
  		end
 
  		def fields(all = false)
- 			api_call(:fields, :get, all ? "all" : nil)
+ 			api_call(resource: :fields, method: :get, all: all)
  		end
+
+    def planting_activities(resource_owner_id)
+      request_params = {
+        :resourceOwnerId => resource_owner_id
+      }
+
+      api_call(resource: :asPlanted, method: :get, request_params: request_params)
+    end
+
+    def planting_activity_raw(planting_activity_id)
+      api_call(resource: :asPlanted, method: :get, id: planting_activity_id, is_binary_body: true)
+    end
 	end
 end
