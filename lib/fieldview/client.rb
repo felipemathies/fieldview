57aad2c5e9217d62112fbe0d9719e1e8803b995c
Robson Leal <robson@checkplant.com.br>
2019-01-31 11:18:45 -0200

    refactoring

--- lib/fieldview/client.rb
+++ lib/fieldview/client.rb
@@ -7,20 +7,9 @@ module Fieldview
 
  		def fields(all = false)
  			api_call(:field, :get, all: all)
- 		end
-
-    def planting_activities(resource_owner_id = nil, occurred_after = nil, occurred_before = nil, updated_after = nil)
-      request_params = {}
-
-      request_params["resourceOwnerId"] = resource_owner_id if resource_owner_id
-      request_params["updatedAfter"]    = updated_after     if updated_after
-      request_params["occurredBefore"]  = occurred_before   if occurred_before
-      request_params["occurredAfter"]   = occurred_after    if occurred_after
-
-      api_call(:plantingActivity, :get, request_params: request_params)
     end
-
-    def harvest_activities(resource_owner_id = nil, occurred_after = nil, occurred_before = nil, updated_after = nil)
+     
+    def activities(resource, resource_owner_id = nil, occurred_after = nil, occurred_before = nil, updated_after = nil)
       request_params = {}
 
       request_params["resourceOwnerId"] = resource_owner_id if resource_owner_id
@@ -28,7 +17,7 @@ module Fieldview
       request_params["occurredBefore"]  = occurred_before   if occurred_before
       request_params["occurredAfter"]   = occurred_after    if occurred_after
 
-      api_call(:harvestActivity, :get, request_params: request_params)
+      api_call(resource.to_sym, :get, request_params: request_params)
     end
     
 	end
--- lib/fieldview/planting_activity.rb
+++ lib/fieldview/planting_activity.rb
@@ -1,30 +1,12 @@
 module Fieldview
-  class PlantingActivity < Fieldview::Base
-
-    attr_accessor :id, :start_time, :end_time, :created_at, :updated_at, :length, :field_ids, :access_token, :raw
+  class PlantingActivity < Fieldview::Activity
 
     def path(id)
       id ? "layers/asPlanted/#{id}/contents" : "layers/asPlanted"
     end
 
-    def initialize(attrs)
-      attrs.each do |key, value|
-        if key == "fields"
-          fields = []
-
-          value.each do |fieldId|
-            fields << Fieldview::Factory.new(:field, { id: fieldId })
-          end
-          
-          instance_variable_set("@#{key.to_s.underscore}", fields)
-        else
-          instance_variable_set("@#{key.to_s.underscore}", value)
-        end
-      end
-    end
-
-    def content
-      self.raw = api_call(:plantingActivity, :get, id: self.id, is_binary_body: true)
+    def get_resource
+      :plantingActivity
     end
   end
 end
