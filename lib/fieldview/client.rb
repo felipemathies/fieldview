6a023440949f698d31f2bf50860539cbbaf11351
Robson <robson@checkplant.com.br>
2019-03-01 10:48:33 -0300

    authorized access method

--- lib/fieldview/client.rb
+++ lib/fieldview/client.rb
@@ -13,11 +13,31 @@ module Fieldview
       request_params = {}
 
       request_params["resourceOwnerId"] = resource_owner_id if resource_owner_id
-      request_params["updatedAfter"]    = updated_after.strftime("%FT%T\Z")     if updated_after
-      request_params["occurredBefore"]  = occurred_before.strftime("%FT%T\Z")   if occurred_before
-      request_params["occurredAfter"]   = occurred_after.strftime("%FT%T\Z")    if occurred_after
+      request_params["updatedAfter"]    = date_formatter(updated_after) if updated_after
+      request_params["occurredBefore"]  = date_formatter(occurred_before) if occurred_before
+      request_params["occurredAfter"]   = date_formatter(occurred_after) if occurred_after
 
       api_call(resource, :get, request_params: request_params)
+    end
+
+    def authorized_access_resource?(resource, resource_owner_id = nil)
+      request_params = {}
+
+      request_params["resourceOwnerId"] = resource_owner_id if resource_owner_id
+      request_params["updatedAfter"]    = date_formatter(Time.now - 10.seconds)
+      
+      begin
+        api_call(resource, :get, request_params: request_params)
+        return true
+      rescue Fieldview::ClientError => exception
+        return false if exception.http_status == 403
+      end
+    end
+
+    private
+
+    def date_formatter(date)
+      date.strftime("%FT%T\Z")
     end
 	end
 end
